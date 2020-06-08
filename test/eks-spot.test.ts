dcceee84287745275fc249433b9cecde43ec387d
Pahud <pahudnet@gmail.com>
2020-06-08 15:05:24 +0800

    minor

--- test/eks-spot.test.ts
+++ test/eks-spot.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as EksSpot from '../lib/eks-spot';
+import * as EksSpot from '../lib';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
