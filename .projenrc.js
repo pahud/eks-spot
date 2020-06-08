a4686eca55b748d3b19d66f3a01bd8c7edfc5af4
Pahud <pahudnet@gmail.com>
2020-06-08 18:38:07 +0800

    minor

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,8 @@ const {
   Semver
 } = require('projen');
 
+const AWS_CDK_LATEST_RELEASE = '1.44.0';
+
 const project = new JsiiProject({
   name: 'eks-spot-blocks',
   jsiiVersion: Semver.caret('1.5.0'),
@@ -12,17 +14,17 @@ const project = new JsiiProject({
   authorEmail: 'hunhsieh@amazon.com',
   stability: 'experimental',
   devDependencies: {
-    '@aws-cdk/assert': Semver.caret('1.44.0'),
+    '@aws-cdk/assert': Semver.caret(AWS_CDK_LATEST_RELEASE),
     'ts-jest': Semver.caret('25.3.1'),
     'jest': Semver.caret('25.5.0'),
   },
   peerDependencies: {
     constructs: Semver.caret('3.0.3'),
-    '@aws-cdk/core': Semver.caret('1.44.0'),
-    '@aws-cdk/aws-ec2': Semver.caret('1.44.0'),
-    '@aws-cdk/aws-eks': Semver.caret('1.44.0'),
-    '@aws-cdk/aws-iam': Semver.caret('1.44.0'),
-    '@aws-cdk/aws-ssm': Semver.caret('1.44.0'),
+    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-ec2': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-eks': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-iam': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-ssm': Semver.caret(AWS_CDK_LATEST_RELEASE),
   },
   python: {
     distName: 'eks-spot-blocks',
