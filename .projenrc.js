66f70bac3c2b00a5d1353f4b4ce937a87765861e
Pahud <pahudnet@gmail.com>
2021-06-07 19:36:46 +0800

    bump projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.82.0';
 const PROJECT_NAME = 'eks-spot-blocks';
@@ -11,6 +11,7 @@ const project = new AwsCdkConstructLibrary({
   repository: 'https://github.com/pahud/cdk-eks-spotblocks.git',
   authorName: 'Pahud Hsieh',
   authorEmail: 'pahudnet@gmail.com',
+  defaultReleaseBranch: 'main',
   keywords: [
     'cdk',
     'aws',
@@ -18,8 +19,16 @@ const project = new AwsCdkConstructLibrary({
     'spot',
     'spot-blocks',
   ],
-  compat: true,
-  dependabot: false,
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud'],
+  },
   catalog: {
     twitter: 'pahudnet',
     announce: false,
@@ -32,88 +41,9 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/aws-iam',
     '@aws-cdk/aws-ssm',
   ],
-  defaultReleaseBranch: 'master',
   python: {
     distName: 'eks-spot-blocks',
     module: 'eks_spot_blocks',
-  },
-});
-
-// create a custom projen and yarn upgrade workflow
-projenYarnUpgrade = project.github.addWorkflow('ProjenYarnUpgrade');
-
-projenYarnUpgrade.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-projenYarnUpgrade.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
-
-// allow manual run this workflow to update the test snapshots
-projenYarnUpgradeUpdateTest = project.github.addWorkflow('ProjenYarnUpgradeUpdateTest');
-
-projenYarnUpgradeUpdateTest.on({
-  workflow_dispatch: {}, // manual trigger only
-});
-
-projenYarnUpgradeUpdateTest.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      { run: 'yarn test' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
   },
 });
 
